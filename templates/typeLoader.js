1ff7e438c707b47f4816dc18ed07471ae20189c5
Zisis Maras <contact@zisismaras.me>
2021-04-01 10:42:49 +0300

    indent fix

--- templates/typeLoader.js
+++ templates/typeLoader.js
@@ -10,29 +10,29 @@ import {accessor as <%= mod.name %>} from "<%= mod.root %>";
 <% } %>
 
 declare module "vue/types/vue" {
-  interface Vue {
-    $store: {
-        <% for (const mod of options.modules) { %>
-        "<%= mod.name %>": ReturnType<typeof <%= mod.name %>>
-        <% } %>
-    };
-  }
+    interface Vue {
+        $store: {
+            <% for (const mod of options.modules) { %>
+            "<%= mod.name %>": ReturnType<typeof <%= mod.name %>>
+            <% } %>
+        };
+    }
 }
 
 declare module "@nuxt/types" {
-  interface Context {
-    $store: {
-        <% for (const mod of options.modules) { %>
-        "<%= mod.name %>": ReturnType<typeof <%= mod.name %>>
-        <% } %>
-    };
-  }
+    interface Context {
+        $store: {
+            <% for (const mod of options.modules) { %>
+            "<%= mod.name %>": ReturnType<typeof <%= mod.name %>>
+            <% } %>
+        };
+    }
 
-  interface NuxtAppOptions {
-    $store: {
-        <% for (const mod of options.modules) { %>
-        "<%= mod.name %>": ReturnType<typeof <%= mod.name %>>
-        <% } %>
-    };
-  }
+    interface NuxtAppOptions {
+        $store: {
+            <% for (const mod of options.modules) { %>
+            "<%= mod.name %>": ReturnType<typeof <%= mod.name %>>
+            <% } %>
+        };
+    }
 }
